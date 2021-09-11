0ea1dd252f30827cd5e59a715a708764f7fcee16
Achu biju <75154223+Achu2234@users.noreply.github.com>
2021-09-11 13:40:12 +0530

    K

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "6"))
-API_HASH = os.getenv("API_HASH", "eb06d4abfb49dc3eeb1aeb98ae0f581e")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-SESSION_NAME = os.getenv("SESSION_NAME")
-ADMIN = os.getenv("ADMIN")
+API_ID = int(os.getenv("API_ID","2107384"))
+API_HASH = os.getenv("API_HASH","43ed88f662a3fcdaf787fef864fb37fa")
+BOT_TOKEN = os.getenv("BOT_TOKEN","1865199109:AAFDmRvuHeyaeyusLiXfD0oFNWI1FmzNuIM")
+SESSION_NAME = os.getenv("SESSION_NAME","BQA5Wsg0rNkRqt9cgP1Xl-vCJLdkgOZjBprApttQQhLRRGI3pPmZFPKC1ats0wyApeIc6ekrQEvrKt1_TPEVzDx-hnGF1CpJZlEjcA4UfEGduLRBPLGByYMqmj1cYSWxuRRqmb5ln5a4ibvnTwimHhqT38g85wi-lXEByxwDayfNjCpgQBfI6TX8xNiHQag34UXb4UIPd6OnV652Q0fleWCHuEVCs-OEng8i9PB2lXI7t_8VvH2eK2tMWev3QCfpWOtf-Jv0zZbqAMXYmi9WPdf_Zo12tMdrCJg0tEtQA-eDEM87Og71Z7odL9nV-Q1TNI2UsBI6qS_FX9Eyh54-KN2RUtHJBAA")
+ADMIN = os.getenv("1221558981")
 DB_URL = os.getenv("DB_URL")
 DB_NAME = os.getenv("DB_NAME")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
